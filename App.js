88e8af4d052377728e916e9dbe44e99fd0167ff0
estevaobarbieri <estevao193@gmail.com>
2023-12-21 13:45:43 -0300

    Criado a HOME e o Sing up

--- App.js
+++ App.js
@@ -1,29 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View, Button, TextInput } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+// import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={{flex: 1, backgroundColor: 'rgb(109, 26, 154)', justifyContent: 'center', alignItems: 'center'}}>
-      <Text style={{color: 'white', fontSize: 55, fontWeight: 'bold'}}>BOOKTOUR</Text>
-      <Text style={{width: 300, color: 'white', fontSize: 20, textAlign: 'center'}}>Explore e organize suas viagens literárias em um só lugar</Text>
-      <View style={{width: 250}}>
-        <Button
-          title="Learn More"
-          color="#841584"
-          accessibilityLabel="Learn more about this purple button"
-        />
-        <Button
-          title="Já sou viajante"
-          color="#841584"
-          accessibilityLabel="Learn more about this purple button"
-        />
-      </View>
-      <TextInput
-        style={{backgroundColor: 'black', color: 'white'}}
-        placeholder="useless placeholder"
-        keyboardType="numeric"
-      />
-      <StatusBar style="auto" />
-    </View>
-  );
+// const App=()=> {
+
+//   return (
+//     <View style={styles.container}>
+//       <Text style={{color: 'white', fontSize: 55, fontWeight: 'bold'}}>BOOKTOUR</Text>
+//       <Text style={{width: 300, color: 'white', fontSize: 20, textAlign: 'center'}}>Explore e organize suas viagens literárias em um só lugar</Text>
+//       <View style={{width: 250}}>
+//         <Button
+//           style={{borderRadius:15, borderWidth:5}}
+//           title="INICIAR 1º VIAGEM"
+//           color="#841584"
+//           accessibilityLabel="Learn more about this purple button"
+//         />
+//         <Button
+//           style={styles.button}
+//           title="Já sou viajante"
+//           color="#841584"
+//           accessibilityLabel="Learn more about this purple button"
+//         />
+//       </View>
+//       <TextInput
+//         style={{backgroundColor: 'black', color: 'white'}}
+//         placeholder="useless placeholder"
+//         keyboardType="numeric"
+//       />
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container:{
+//     backgroundColor: "black",
+
+//   },
+//   button: {
+//     backgroundColor: "#0000",
+//     borderRadius: 15,
+
+//   }
+
+// })
+
+// export default App;
+
+import Home from './Screens/Home'
+
+const App = () => {
+  return <Home/>
 }
+export default App
